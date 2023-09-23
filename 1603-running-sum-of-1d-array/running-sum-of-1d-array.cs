7a4a8c2ba25e4042b87f535bc4db0c940efa5e03
zabuli <131911322+zabuli@users.noreply.github.com>
2023-09-23 14:50:59 +0200

    Time: 139 ms (16.08%) | Memory: 43.2 MB (88.94%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        for(var i = 1; i < nums. Length; i++)
+        {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+    }
+}
