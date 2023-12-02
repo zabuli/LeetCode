8aa8681a2e6ba2cb35ca66a8cbbf03dfec6f3ba7
Maria Nemsilajova <131911322+zabuli@users.noreply.github.com>
2023-12-02 18:59:02 +0100

    Time: 73 ms (97.22%) | Memory: 40.4 MB (55.56%) - LeetSync

--- 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.cs
+++ 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.cs
@@ -3,40 +3,38 @@ public class Solution {
         Array.Sort(arr2);
 
         var result = 0;
-        int min; int max; int mid;
-        bool isDistance;
         for(var i = 0; i < arr1.Length; i++)
         {
-            min = 0; 
-            max = arr2.Length - 1;
-            isDistance = true;
-            //4 5 8
-            //1 8 9 10
-            //2
-            while(min <= max)
-            {
-                mid = (min + max) / 2;
-                if (Math.Abs(arr1[i] - arr2[mid]) <= d)
-                {
-                    isDistance = false;
-                    break;
-                }
-                else if (arr1[i] > arr2[mid])
-                {
-                    min = mid + 1;
-                }
-                else
-                {
-                    max = mid - 1;
-                }
-            }
-
-            if (isDistance)
-            {
-                result++;
-            }
+            result += GetDistanceValue(arr1, arr2, d, i);
         }
 
         return result;
+    }
+
+    private int GetDistanceValue(int[] arr1, int[] arr2, int d, int i)
+    {
+        var min = 0; var mid = 0;
+        var max = arr2.Length - 1;
+        var isDistance = false;
+
+        while(min <= max)
+        {
+            mid = (min + max) / 2;
+            if (Math.Abs(arr1[i] - arr2[mid]) <= d)
+            {
+                isDistance = true;
+                break;
+            }
+            else if (arr1[i] > arr2[mid])
+            {
+                min = mid + 1;
+            }
+            else
+            {
+                max = mid - 1;
+            }
+        }
+        
+        return isDistance ? 0 : 1;
     }
 }
