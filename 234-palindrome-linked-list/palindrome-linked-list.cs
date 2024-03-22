4c0f6a84f879bc673ade7570624a66e2a3a5f7a9
Maria Nemsilajova <131911322+zabuli@users.noreply.github.com>
2024-03-22 14:23:38 +0000

    Time: 322 ms (48.12%) | Memory: 67.8 MB (75.44%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cs
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+        {
+            return true;
+        }
+
+        var slow = head;//1
+        var fast = head.next;//2
+        var stack = new Stack<ListNode>();
+        stack.Push(slow);
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;//2,3,4,5
+            stack.Push(slow);
+
+            fast = fast.next.next;//4,5,3,1
+        }
+
+        if (fast == null)
+        {
+            stack.Pop();
+        }
+        if (stack.Count == 0)
+        {
+            return false;
+        }
+
+        while (slow != null && stack.Count > 0)
+        {
+            slow = slow.next;
+            
+            if (stack.Pop().val != slow.val)
+            {
+                return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
