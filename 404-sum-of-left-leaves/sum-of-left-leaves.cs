1937e7d44122d6e5341db7c6d3706d485457fccc
Maria Nemsilajova <131911322+zabuli@users.noreply.github.com>
2024-04-14 21:01:25 +0200

    Time: 64 ms (36.82%) | Memory: 40.9 MB (66.17%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int SumOfLeftLeaves(TreeNode root) {
+        var sum = 0;
+
+        if (root == null)
+        {
+            return sum;
+        }
+
+        if (root.left != null && root.left.left == null && root.left.right == null)
+        {
+            sum += root.left.val; 
+        }
+
+        sum += SumOfLeftLeaves(root.left);
+        sum += SumOfLeftLeaves(root.right); 
+
+        return sum;
+    }
+}
