7ba5c4c1a382822355e52e36a1d1726a52f227b1
Maria Nemsilajova <131911322+zabuli@users.noreply.github.com>
2024-07-28 14:24:29 +0200

    Time: 40 ms (99.07%) | Memory: 39.3 MB (40.64%) - LeetSync

--- 290-word-pattern/word-pattern.cs
+++ 290-word-pattern/word-pattern.cs
@@ -1,7 +1,6 @@
 public class Solution {
     public bool WordPattern(string pattern, string s) {
         var hashMap = new Dictionary<char, string>();
-        var usedWords = new HashSet<string>();
         var words = s.Split(" ");
 
         if (pattern.Length != words.Length)
@@ -11,7 +10,7 @@ public class Solution {
 
         for (var i = 0; i < pattern.Length; i++)
         {
-            if (usedWords.Contains(words[i]))
+            if (hashMap.ContainsValue(words[i]))
             {
                 if (!hashMap.ContainsKey(pattern[i]) || !hashMap[pattern[i]].Equals(words[i]))
                 {
@@ -22,7 +21,6 @@ public class Solution {
             if (!hashMap.ContainsKey(pattern[i]))
             {
                 hashMap.Add(pattern[i], words[i]);
-                usedWords.Add(words[i]);
             }
             else if (!hashMap[pattern[i]].Equals(words[i]))
             {
