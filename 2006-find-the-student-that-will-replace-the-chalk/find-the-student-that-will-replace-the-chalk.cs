8c0868c62b16feb289fc1caad1bd859a3be51cd1
Maria Nemsilajova <131911322+zabuli@users.noreply.github.com>
2024-09-02 09:34:24 +0200

    Time: 323 ms (73.33%) | Memory: 60.1 MB (93.33%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cs
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cs
@@ -1,26 +1,38 @@
 public class Solution {
     public int ChalkReplacer(int[] chalk, int k) {
-        var i = 0;
+        var prefixSum = GetPrefixSum(chalk);
 
-        while (k > 0)
+        k = (int)(k % prefixSum[^1]);
+
+        var min = 0;
+        var max = chalk.Length - 1;
+
+        while (min <= max)
         {
-            if (chalk[i] <= k)
+            var mid = min + ((max - min) / 2);
+            if (prefixSum[mid] > k)
             {
-                k = k - chalk[i];
+                max = mid - 1;
             }
             else
             {
-                return i;
-            }
-
-            i++;
-
-            if (i > chalk.Length - 1)
-            {
-                i = 0;
+                min = mid + 1;
             }
         }
 
-        return i;
+        return min;
+    }
+
+    private static long[] GetPrefixSum(int[] chalk)
+    {
+        var prefixSum = new long[chalk.Length];
+        prefixSum[0] = chalk[0];
+
+        for (var i = 1; i < chalk.Length; i++)
+        {
+            prefixSum[i] = chalk[i] + prefixSum[i - 1];
+        }
+
+        return prefixSum;
     }
 }
